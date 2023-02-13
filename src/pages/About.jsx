c037d2a11c814f82ce416392c68208f3efafa9a4
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-02-13 17:06:08 -0300

    changed about text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ function About() {
       <article className="about-page">
         <h2>About</h2>
         <p>
-          This is a free API, check more about here
+          This is a free API, for more information, check here:
           <a href="https://www.thecocktaildb.com/" target='_blank'>
           https://www.thecocktaildb.com
           </a>
